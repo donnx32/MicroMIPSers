465f0dc1947a0d383f0bd8b61379dbf6414e7525
gracielnunciob <36474948+gracielnunciob@users.noreply.github.com>
2018-04-10 00:00:02 +0800

    Add files via upload

--- MicroMips/src/model/CyclesModel.java
+++ MicroMips/src/model/CyclesModel.java
@@ -12,6 +12,8 @@ public class CyclesModel {
 	String Rn;
 	Instruction instruction;
 	
+	Cycle cycle = new Cycle();
+	
 	public CyclesModel(Instruction instruction)
 	{
 		this.instruction = instruction;
@@ -19,142 +21,163 @@ public class CyclesModel {
 	
 	public void Assign()
 	{
-		IR = instruction.getHex();
-	//  NPC = get memory+4;
-		A = instruction.getB25to21();
-		B = instruction.getB20to16();
-		IMM = instruction.getB15to11() + instruction.getB10to6() + instruction.getB5to0();
+		double address = 0;
+		
+		address = convertToDec(address);
+		address = address + 4;
+		
+		cycle.setiR(instruction.getHex());
+		cycle.setnPC(Double.toHexString(address));
+		cycle.setA(instruction.getBin().substring(7, 11));
+		cycle.setB(instruction.getBin().substring(12, 16));
+		cycle.setiMM(instruction.getBin().substring(17, 32));
 		
 		//LD
-		if (instruction.equals("110111"))
+		if (instruction.getBin().substring(1, 6).equals("110111"))
 		{
-			int temp1 = Integer.parseInt(A);
-			int temp2 = Integer.parseInt(IMM);
+			int temp1 = Integer.parseInt(cycle.getA());
+			int temp2 = Integer.parseInt(cycle.getiMM());
 			
 			int result = temp1 + temp2;			
 			
-			ALUOUTPUT = String.format ("%016d", result);
+			cycle.setaLUOUTPUT(String.format ("%016d", result));
 			
-			COND = 0;
+			cycle.setcOND("0");
 			
-			PC = NPC;
+			cycle.setpC(cycle.getnPC());;
 		//	LMD = DEPENDE SA MEMORY
-			RANGE = "N/A";
-		//  Rn = LMD 
+			cycle.setRange("N/A");
+			cycle.setrN(cycle.getlMD());
 			
 		}
 		
 		//SD
-		else if (instruction.equals("111111"))
+		else if (instruction.getBin().substring(1, 6).equals("111111"))
 		{
-			int temp1 = Integer.parseInt(A);
-			int temp2 = Integer.parseInt(IMM);
+			int temp1 = Integer.parseInt(cycle.getA());
+			int temp2 = Integer.parseInt(cycle.getiMM());
 			
 			int result = temp1 + temp2;			
 			
-			ALUOUTPUT = String.format ("%016d", result);
+			cycle.setaLUOUTPUT(String.format ("%016d", result));
+			cycle.setcOND("0");
 			
-			COND = 0;
-			
-			PC = NPC;
-			LMD = "N/A";
+			cycle.setpC(cycle.getnPC());;
+			cycle.setlMD("N/A");
 		// RANGE = HOW MANY SPACES SA ALUPOUTPUT
-		  Rn = "N/A";
 			
+			cycle.setrN("N/A");
 		}
 		
 		//DADDIU
-		else if (instruction.equals("011001"))
+		else if (instruction.getBin().substring(1, 6).equals("011001"))
 		{
-			int temp1 = Integer.parseInt(A);
-			int temp2 = Integer.parseInt(IMM);
+			int temp1 = Integer.parseInt(cycle.getA());
+			int temp2 = Integer.parseInt(cycle.getiMM());
 			
 			int result = temp1 + temp2;			
 			
-			ALUOUTPUT = String.format ("%016d", result);
+			cycle.setaLUOUTPUT(String.format ("%016d", result));
+			cycle.setcOND("0");
 			
-			COND = 0;
+			cycle.setpC(cycle.getnPC());;
+			cycle.setlMD("N/A");
+			cycle.setRange("N/A");
 			
-			PC = NPC;
-			LMD = "N/A";
-			RANGE = "N/A";
-			Rn = ALUOUTPUT;
-			
+			cycle.setrN(cycle.getaLUOUTPUT());
 			
 		}
 		
 		//DADDU
-		else if (instruction.equals("000000"))
+		else if (instruction.getBin().substring(1, 6).equals("000000"))
 		{
-			int temp1 = Integer.parseInt(A);
-			int temp2 = Integer.parseInt(B);
+			int temp1 = Integer.parseInt(cycle.getA());
+			int temp2 = Integer.parseInt(cycle.getB());
 			
 			int result = temp1 + temp2;			
 			
-			ALUOUTPUT = String.format ("%016d", result);
+			cycle.setaLUOUTPUT(String.format ("%016d", result));
+			cycle.setcOND("0");
 			
-			COND = 0;
+			cycle.setpC(cycle.getnPC());;
+			cycle.setlMD("N/A");
+			cycle.setRange("N/A");
 			
-			PC = NPC;
-			LMD = "N/A";
-			RANGE = "N/A";
-			Rn = ALUOUTPUT;
-			
+			cycle.setrN(cycle.getaLUOUTPUT());	
 		}
 		
 		//BC
-		else if (instruction.equals("110010"))
+		else if (instruction.getBin().substring(1, 6).equals("110010"))
 		{
 		
 		//	ALUOUTPUT = Sir's formula;
-			
-			COND = 1;
+			cycle.setcOND("1");
 			
 		//	PC = Address Where the Jump is;
-			LMD = "N/A";
-			RANGE = "N/A";
-			Rn = "N/A";	
+			cycle.setlMD("N/A");
+			cycle.setRange("N/A");
+			
+			cycle.setrN("N/A");	
 		}
 		
+		
 		//BNEZC
-		else if (instruction.equals("111110"))
+		else if (instruction.getBin().substring(1, 6).equals("111110"))
 		{
-//			ALUOUTPUT = Sir's formula;
-			
-			if (instruction.getB25to21().equals("00000"))
-				COND = 0;
-			
+			//ALUOUTPUT = Sir's formula;
+			//If A is 0 then tru
+			if (cycle.getA().equals("00000"))
+				cycle.setcOND("0");
 			else 
-				COND = 1;
+				cycle.setcOND("1");;
 					
-		//	PC = Where the Jump is;
-			LMD = "N/A";
-			RANGE = "N/A";
-			Rn = "N/A";	
+			//PC = Where the Jump is;
+			cycle.setlMD("N/A");
+			cycle.setRange("N/A");
+				
+			cycle.setrN("N/A");	
 							
 		}
 		
 		//ORI
-		else if (instruction.equals("111110"))
+		else if (instruction.getBin().substring(1, 6).equals("111110"))
 		{
-			int temp1 = Integer.parseInt(A);
-			int temp2 = Integer.parseInt(IMM);
+			int temp1 = Integer.parseInt(cycle.getA());
+			int temp2 = Integer.parseInt(cycle.getiMM());
 			
 			int result = temp1 + temp2;			
 			
-			ALUOUTPUT = String.format ("%016d", result);
+			cycle.setaLUOUTPUT(String.format ("%016d", result));
+			cycle.setcOND("0");
 			
-			COND = 0;
-			
-			PC = NPC;
-			LMD = "N/A";
-			RANGE = "N/A";
-			Rn = ALUOUTPUT;
+			//PC = Where the Jump is;
+			cycle.setlMD("N/A");
+			cycle.setRange("N/A");
+				
+			cycle.setrN(cycle.getaLUOUTPUT());	
 							
 		}
 		
 	}
 	
 	
+	public double convertToDec(double address)
+	{
+		//turns hex to dec
+		 if(instruction.getHex().contains("A") ||instruction.getHex().contains("B") || 
+			instruction.getHex().contains("C") || instruction.getHex().contains("D") || 
+			instruction.getHex().contains("E") ||instruction.getHex().contains("F"))
+		 {   
+			 address  = Integer.parseInt(instruction.getHex(), 16);
+			 address = (double) address;
+		 }
+		 
+		 else
+		 {
+			 address = Double.parseDouble(String.valueOf(instruction.getHex()));
+		 }
+		 
+		 return address;
+	}
 
 }

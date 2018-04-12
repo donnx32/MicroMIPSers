e86030d892c5e63c6dc4bb547f49153cb25c7dd2
donnx32 <32759912+donnx32@users.noreply.github.com>
2018-04-13 01:57:46 +0800

    irv to view

--- MicroMips/src/view/InputRegisterView.java
+++ MicroMips/src/view/InputRegisterView.java
@@ -1,12 +1,14 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
@@ -35,7 +37,7 @@ public class InputRegisterView extends JFrame {
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 		setResizable(false);
-		
+
 		initializeComponents();
 		generateListeners();
 	}
@@ -46,7 +48,7 @@ public class InputRegisterView extends JFrame {
 		panel.setBounds(10, 11, 274, 129);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		pnlBtnOK = new JPanel();
 		pnlBtnOK.setBackground(new Color(203, 201, 201));
 		pnlBtnOK.setBounds(25, 88, 100, 30);
@@ -90,13 +92,47 @@ public class InputRegisterView extends JFrame {
 		pnlBtnOK.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				Register r = RegisterModel.regList.get(Integer.parseInt(lblReg.getText()));
-				r.setRegisterValue(textField.getText());
-				MainView.getrModel().setValueAt(r.getRegisterValue(), r.getRegisterNumber(), 1);
-				
-				lblReg.setText("");
-				textField.setText("");
-				setVisible(false);
+
+				Component frame = null;
+				String str = textField.getText().replaceAll("\\s+", "");
+				int err = 0;
+
+				System.out.println(str);
+
+				if (str.length() > 16) {
+					System.out.println("error in length");
+					JOptionPane.showMessageDialog(frame, "Error: Maximum Number of 16 Digits");
+					lblReg.setText("");
+					textField.setText("");
+					setVisible(false);
+				}
+
+				else {
+					str = zeroExtend(str, 16);
+					System.out.println(str);
+					for (int i = 0; i < str.length(); i++)
+						if (str.charAt(i) > 'F')
+							err++;
+
+					if (err > 0) {
+						System.out.println("error in validity");
+						JOptionPane.showMessageDialog(frame, "Error: Input is not Valid");
+						lblReg.setText("");
+						textField.setText("");
+						setVisible(false);
+					}
+
+					else {
+						Register r = RegisterModel.regList.get(Integer.parseInt(lblReg.getText()));
+						r.setRegisterValue(str);
+						MainView.getrModel().setValueAt(r.getRegisterValue(), r.getRegisterNumber(), 1);
+
+						lblReg.setText("");
+						textField.setText("");
+						setVisible(false);
+					}
+				}
+
 			}
 		});
 
@@ -113,4 +149,22 @@ public class InputRegisterView extends JFrame {
 	public JLabel getLblReg() {
 		return lblReg;
 	}
+
+	public String zeroExtend(String s, int n) {
+		StringBuilder sb = null;
+
+		if (s.length() < n) {
+			sb = new StringBuilder();
+			int diff = n - s.length();
+
+			for (int i = 0; i < diff; i++) {
+				sb.append("0");
+			}
+
+			return sb.toString() + "" + s;
+		} else {
+			return s.substring(s.length() - n, s.length());
+		}
+	}
+
 }
